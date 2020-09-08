3e165a1abaa4b8a7010d384dd22bd80197b58660
Pushp Raj <pushp277@gmail.com>
2020-09-08 20:26:29 +0530

    icon is added

---
+++ index.php
@@ -0,0 +1,15 @@
+ 
+<!DOCTYPE html>
+<html>
+<head>
+<title> The Convergence Team</title>
+<meta description="The Convergence Team founds on 13.8.2020 a great initiative by Mr.Pushp Raj, Mr.Saurabh Mukherjee, Mr.Shivam Rai and Mr.Nikhil Sawan" author="tct">
+<link rel="icon" href="logo.png" type="image/x-icon">
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+
+write your code here
+
+</body>
+</html>
