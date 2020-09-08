b5cc4b0d638bd52e25c416f41df4b45bb34a5b6a
Pushp Raj <64361848+pushp277@users.noreply.github.com>
2020-09-08 20:13:21 +0530

    Update index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> The Convergence Team</title>
+<meta description="The Convergence Team founds on 13.8.2020 a great initiative by Mr.Pushp Raj, Mr.Saurabh Mukherjee, Mr.Shivam Rai and Mr.Nikhil Sawan" author="tct">
+<link rel="icon" href="https://github.com/pushp277/convergence.git" type="image/x-icon">
+</head>
+<body>
+
+
+</body>
+</html>
